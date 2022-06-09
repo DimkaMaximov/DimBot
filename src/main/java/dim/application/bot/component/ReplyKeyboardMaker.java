bbb2721dacf0dafb97a31787f92fedab18739e39
dvmaksimov <dvmaksimov@phoenixit.ru>
2022-06-09 17:15:33 +0300

    Бот перешел на использование Spring Boot.
    Новый функционал:
      После вычисления петуха добавлена инлайн-клавиатура:
        - Согласиться (с итогом вычисления)
        - Крути барабан заново

---
+++ src/main/java/dim/application/bot/component/ReplyKeyboardMaker.java
@@ -0,0 +1,36 @@
+package dim.application.bot.component;
+
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class ReplyKeyboardMaker {
+
+    public ReplyKeyboardMarkup getMainMenuKeyboard() {
+
+        KeyboardRow row1 = new KeyboardRow();
+        row1.add(new KeyboardButton(ButtonNameEnum.IDENTIFY_ROOSTER.getButtonName()));
+        row1.add(new KeyboardButton(ButtonNameEnum.IDENTIFY_PRETTY.getButtonName()));
+
+        KeyboardRow row2 = new KeyboardRow();
+        row2.add(new KeyboardButton(ButtonNameEnum.DO_SOMETHING.getButtonName()));
+        row2.add(new KeyboardButton(ButtonNameEnum.SEND_ME_CAT.getButtonName()));
+
+        List<KeyboardRow> keyboard = new ArrayList<>();
+        keyboard.add(row1);
+        keyboard.add(row2);
+
+        final ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        replyKeyboardMarkup.setKeyboard(keyboard);
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(true);
+
+        return replyKeyboardMarkup;
+    }
+}
